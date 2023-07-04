07cee6355ce74021735eacc72a468adc1f0227e7
itsproali <itsproali@gmail.com>
2023-07-04 18:28:07 +0600

    Logs Removed

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,8 @@
-import { createLogger, format, transports } from "winston";
+import { createLogger, format, transports, Logger } from "winston";
 const { combine, timestamp, printf, colorize, prettyPrint } = format;
+import DailyRotateFile from "winston-daily-rotate-file";
 
+// Format log messages
 const myFormat = printf(({ level, message }) => {
   const date = new Date().toLocaleDateString("en-US", {
     hour: "numeric",
@@ -10,7 +12,16 @@ const myFormat = printf(({ level, message }) => {
   return `${level}: ${message} ${date}`;
 });
 
-const logger = createLogger({
+// const transport: DailyRotateFile = new DailyRotateFile({
+//   filename: "logs/%DATE%.log",
+//   datePattern: "YYYY-MM-DD-HH",
+//   zippedArchive: true,
+//   maxSize: "20m",
+//   maxFiles: "1d",
+// });
+
+// Create logger
+const logger: Logger = createLogger({
   level: "info",
   format: combine(
     timestamp(),
@@ -23,8 +34,25 @@ const logger = createLogger({
     new transports.Console({
       format: combine(colorize(), timestamp(), myFormat),
     }),
-    new transports.File({ filename: "logs/info.log", level: "info" }),
-    new transports.File({ filename: "logs/error.log", level: "error" }),
+    // new transports.File({ filename: "logs/info.log", level: "info" }),
+    // new transports.File({ filename: "logs/error.log", level: "error" }),
+    // new transports.File({ filename: "logs/war.log", level: "warn" }),
+    new DailyRotateFile({
+      filename: "logs/combined-%DATE%.log",
+      datePattern: "YYYY-MM-DD-HH",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "1d",
+      level: "info",
+    }),
+    new DailyRotateFile({
+      filename: "logs/error-%DATE%.log",
+      datePattern: "YYYY-MM-DD-HH",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "1d",
+      level: "error",
+    }),
   ],
 });
 
